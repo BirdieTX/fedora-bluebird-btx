abb0645edd8d0aea925eb1990d1c57b91fb8d188
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-08-30 21:23:41 -0500

    commit

--- files/scripts/priority.sh
+++ files/scripts/priority.sh
@@ -7,9 +7,6 @@ dnf5 config-manager setopt \
     fedora.priority=99 \
     updates.priority=98 \
     fedora-cisco-openh264.priority=97 \
-    rpmfusion-free.priority=89 \
-    rpmfusion-nonfree.priority=88 \
-    rpmfusion-free-updates.priority=87 \
-    rpmfusion-nonfree-updates.priority=86 \
-    terra.priority=79 \
-    terra-mesa.priority=78
+    terra.priority=89 \
+    fedora-multimedia.priority=88 \
+    terra-mesa.priority=87
