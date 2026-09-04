ca19c37c97a46b6a0ed3ba70b000f9098a5285d2
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-09-03 19:50:17 -0500

    Commit

--- files/scripts/post.sh
+++ files/scripts/post.sh
@@ -3,6 +3,14 @@
 set -oue pipefail
 
 dnf5 config-manager setopt '*.enabled=0'
+dnf5 config-manager setopt \
+    copr:copr.fedorainfracloud.org:ublue-os:packages.enabled=1 \
+    fedora.enabled=1 \
+    updates.enabled=1 \
+    fedora-cisco-openh264.enabled=1 \
+    terra.enabled=1 \
+    fedora-multimedia.enabled=1 \
+    terra-mesa.enabled =1
 dconf update
 systemctl disable NetworkManager-wait-online.service
 systemctl enable grub-config.service
--- files/scripts/priority.sh
+++ files/scripts/priority.sh
@@ -4,9 +4,22 @@ set -oue pipefail
 
 dnf5 config-manager setopt \
     copr:copr.fedorainfracloud.org:ublue-os:packages.priority=109 \
+    copr:copr.fedorainfracloud.org:ublue-os:akmods.priority=108 \
+    updates-archive.priority=100 \
     fedora.priority=99 \
     updates.priority=98 \
     fedora-cisco-openh264.priority=97 \
     terra.priority=89 \
     fedora-multimedia.priority=88 \
     terra-mesa.priority=87
+
+dnf5 config-manager setopt \
+    updates-archive.name='Fedora Updates (Archive)'
+    copr:copr.fedorainfracloud.org:ublue-os:packages.name='Universal Blue' \
+    copr:copr.fedorainfracloud.org:ublue-os:akmods.name='Nvidia Drivers'
+    fedora.name='Fedora Core' \
+    updates.name='Fedora Updates' \
+    fedora-cisco-openh264.name='Cisco OpenH264' \
+    terra.name='Fyra Labs Terra' \
+    fedora-multimedia.name='Fedora Multilib' \
+    terra-mesa.name='Fyra Labs Mesa'
